e5c932c9bbcb597bae0ca71683ffa45f4add73c5
Jonas Jensen <jonaskruse123@gmail.com>
2024-05-15 10:38:27 +0200

    Revert "test af fonts i html og css"

--- gasa/src/views/HomeView.vue
+++ gasa/src/views/HomeView.vue
@@ -1,16 +1,9 @@
-
-
 <script setup>
-import mainpage from '@/Components/mainpage.vue';
-import navbar from '@/Components/navbar.vue';
 
 </script>
 
 <template>
   <main>
-   <mainpage />
-   <navbar />
-
-
+   
   </main>
 </template>
