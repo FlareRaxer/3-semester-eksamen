c94b28de835bdb580259a64c4cd62069d40af85b
Bo7913DK <bohkuntz@gmail.com>
2024-05-16 10:15:17 +0200

    components undermappe test

--- gasa/src/Components/navbar.vue
+++ gasa/src/Components/navbar.vue
@@ -1,20 +1,20 @@
 <template>
-
- <nav>
-  <ul id="menu">
-    <li>VELKOMMEN TIL G.A.S.A.</li>
-    <li>JULEFROKOST</li>
-    <li>VIRTUEL RUNDTUR</li>
-    <li>STAND-UP EVENTS</li>
-    <li>TIL VIRKSOMHEDER</li>
-    <li>FIRMA- OG PRIVATARRANGEMENTER</li>
-    <li>PRIVATE SELSKABER OG BEGIVENHEDER</li>
-    <li>BOOK G.A.S.A.'S LOKALER</li>
-    <li>TINDERBRUNCH // TINDERBOX</li>
-    <li>RESTAURANT HALLUNDSBÆK</li>
-    <li>PRAKTISK INFO</li>
-  </ul>
- </nav>
-  
-
-</template>
+    <div class="sidebar">
+     <h2>GASA</h2>
+     <nav>
+      <ul id="menu">
+        <li>VELKOMMEN TIL G.A.S.A.</li>
+        <li>JULEFROKOST</li>
+        <li>VIRTUEL RUNDTUR</li>
+        <li>STAND-UP EVENTS</li>
+        <li>TIL VIRKSOMHEDER</li>
+        <li>FIRMA- OG PRIVATARRANGEMENTER</li>
+        <li>PRIVATE SELSKABER OG BEGIVENHEDER</li>
+        <li>BOOK G.A.S.A.'S LOKALER</li>
+        <li>TINDERBRUNCH // TINDERBOX</li>
+        <li>RESTAURANT HALLUNDSBÆK</li>
+        <li>PRAKTISK INFO</li>
+      </ul>
+     </nav>
+    </div>
+    </template>
