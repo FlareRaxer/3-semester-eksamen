b7349e7f3f57d7bce47c55caaa6dc8b707885801
Bo7913DK <bohkuntz@gmail.com>
2024-05-22 21:47:43 +0200

    Lavet footer (i mainpage.vue)
    Footeren er lavet i "mainpage.vue", da der var problemer med at få footeren til at vises korrekt på hjemmesiden i footeren.vue. "Footeren.vue" kan slettes, hvis footer ikke senere bliver flyttet herover.

--- gasa/src/Components/mainpage.vue
+++ gasa/src/Components/mainpage.vue
@@ -18,4 +18,39 @@
           <p>Indsæt tekst her</p>
       </section>
   </div>
+  <div class="footer">
+    <div class="footer-box">
+     <img src="@/img/gasa_logo_bittergreen.png" class="footer-gasa-logo">
+     <div class="footer-info-grid">
+      <div class="footer-grid-left">
+        <h3 class="footer-h3">KONTAKT OS:</h3>
+        <p class="footer-text"><img src="@/img/phone.png" class="footer-text-icon">31114045</p>
+        <p class="footer-text">Kontakt for events og arrangementer:
+          <br>
+          <img src="@/img/envelope.png" class="footer-text-icon">info@oplevgasa.dk
+        </p>
+        <p class="footer-text">Kontakt for Restaurant Hallundsbæk:
+          <br>
+          <img src="@/img/envelope.png" class="footer-text-icon">info@hallundsbaeks.dk
+        </p>
+      </div>
+      <div class="footer-grid-middle">
+        <h3 class="footer-h3">TELEFONTID</h3>
+        <p class="footer-text footer-text-bold">Man-fre: 09:00-15:00</p>
+        <p class="footer-text footer-text-bold">Middelfartvej 9M,<br>5000 Odense</p>
+        <p class="footer-text footer-text-bold">CVR: 42024686<br>G.A.S.A. A/S</p>
+        <img src="@/img/kontrolrapport.png" class="footer-kontrolrapport">
+      </div>
+      <div>
+        <h3 class="footer-h3">BETALING:</h3>
+        <p class="footer-text">Der kan betales med kort og kontanter.<br>Vi tager ikke imod Mobilepay.</p>
+        <p></p>
+        <h3 class="footer-h3">FIND VEJ & PARKERING</h3>
+        <p class="footer-text">Handelsbetingelser<br>Cookie, Persondata & Privatlivs-politik</p>
+        <h3 class="footer-h3">FIND OS PÅ EVARTO</h3>
+        <img src="@/img/evarto.png" class="footer-evarto">
+      </div>
+     </div>
+   </div>
+  </div>
 </template>
