29cc989a5546b93f7d29dc99ef8154478408f71c
Bo7913DK <bohkuntz@gmail.com>
2024-05-22 12:12:47 +0200

    Rettet class-navne i navbar.vue og dertilhørende classes i main.css fra at være snake_case til at være kebab-case

--- gasa/src/Components/navbar.vue
+++ gasa/src/Components/navbar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="sidebar">
    <div>
-    <img src="@/img/gasa_logo.png" class="logo_sidebar">
+    <img src="@/img/gasa_logo.png" class="logo-sidebar">
    </div>
    <nav>
-    <ul class="navbar_list">
+    <ul class="navbar-list">
       <li>VELKOMMEN TIL G.A.S.A.</li>
       <li>JULEFROKOST</li>
       <li>VIRTUEL RUNDTUR</li>
       <li>STAND-UP EVENTS</li>
       <li>TIL VIRKSOMHEDER</li>
-      <li class="li_selected">FIRMA- OG PRIVATARRANGEMENTER</li>
+      <li class="li-selected">FIRMA- OG PRIVATARRANGEMENTER</li>
       <li>PRIVATE SELSKABER OG BEGIVENHEDER</li>
       <li>BOOK G.A.S.A.'S LOKALER</li>
       <li>TINDERBRUNCH // TINDERBOX</li>
@@ -18,19 +18,19 @@
       <li>PRAKTISK INFO</li>
     </ul>
    </nav>
-   <hr class="sidebar_line">
-   <div class="sidebar_fb_ig">
+   <hr class="sidebar-line">
+   <div class="sidebar-fb-ig">
     <div>
-     <img src="@/img/icon_facebook.svg" class="some_icon">
-     <p class="some_text">FACEBOOK</p>
+     <img src="@/img/icon_facebook.svg" class="some-icon">
+     <p class="some-text">FACEBOOK</p>
     </div>
     <div>
-      <img src="@/img/icon_instagram.svg" class="some_icon">
-     <p class="some_text">INSTAGRAM</p>
+      <img src="@/img/icon_instagram.svg" class="some-icon">
+     <p class="some-text">INSTAGRAM</p>
     </div>
    </div>
-   <div class="bargroup_logo_flex">
-    <img src="@/img/bargroup_logo.png" class="bargroup_logo">
+   <div class="bargroup-logo-flex">
+    <img src="@/img/bargroup_logo.png" class="bargroup-logo">
    </div>
   </div>
   </template>
